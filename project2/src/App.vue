ec0ab80b9743e33a90ef872d16b0d7850b362317
mkae21 <bbuchu123@gmail.com>
2024-11-20 19:33:44 +0900

    Update App.vue
    app.vue 변경

--- project2/src/App.vue
+++ project2/src/App.vue
@@ -1,53 +1,24 @@
 <template>
   <div id="app">
-    <!-- Header 컴포넌트를 추가하여 최상단에 위치시킵니다 -->
-    <Header />
-    <!-- 라우트된 페이지를 렌더링하는 부분 -->
+    <!-- Angular의 <router-outlet>에 대응하는 Vue의 <router-view> -->
     <router-view />
   </div>
 </template>
 
 <script>
-import Header from './layouts/MyHeader.vue'
-
 export default {
   name: 'App',
-  components: {
-    Header
+  data () {
+    return {
+      title: 'Angular-demo' // Angular의 title 속성
+    };
   },
-  computed: {
-    user () {
-      return this.$store.state.user
-    }
+  mounted () {
+    console.log(`App loaded with title: ${this.title}`);
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: left;
-  color: #fafafa;
-  padding-top: 30px; /* Header 높이에 맞춰 조정 */
-  /* 헤더와 padding left 맞추기 */
-  padding-left: 40px;
-  /* 볼드체 */
-  font-weight: bold;
-
-}
-
-nav {
-  padding: 10px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped>
+/* app.component.css에 해당하는 스타일을 여기에 추가 */
 </style>
