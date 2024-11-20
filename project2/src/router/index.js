7ac0f123aad3b3e001205932ee2cd0701a8baf72
mkae21 <bbuchu123@gmail.com>
2024-11-21 02:24:53 +0900

    Update index.js
    router 추가

--- project2/src/router/index.js
+++ project2/src/router/index.js
@@ -1,27 +1,42 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeMainView from '@/views/HomeMain.vue'
-import HomeSineIn from '@/components/Sine-in.vue'
+import HomeComponent from '../components/home/HomeComponent.vue'
+import HomeMainComponent from '../components/home/main/HomeMainComponent.vue'
+import SignInComponent from '../components/sign-in/SignInComponent.vue'
+import HomePopularComponent from '../components/home/popular/HomePopularComponent.vue'
+import HomeWishlistComponent from '../components/home/wishlist/HomeWishlistComponent.vue'
+import HomeSearchComponent from '../components/search/HomeSearchComponent.vue'
+
 const routes = [
   {
-    path: '/',
-    name: 'SineIn',
-    component: HomeSineIn
+    path: '/', // 부모 컴포넌트 경로
+    component: HomeComponent,
+    children: [
+      {
+        path: '', // 부모 경로에 상대적인 기본 자식 경로
+        component: HomeMainComponent
+      },
+      {
+        path: 'popular', // 부모 경로에 상대적인 'popular' 경로
+        component: HomePopularComponent
+      },
+      {
+        path: 'wishlist', // 부모 경로에 상대적인 'popular' 경로
+        component: HomeWishlistComponent
+      },
+      {
+        path: 'search',
+        component: HomeSearchComponent
+      }
+    ]
   },
   {
-    path: '/home',
-    name: 'home',
-    component: HomeMainView
-  },
-  {
-    path: '/popular',
-    name: 'popular',
-    component: HomeMainView
+    path: '/signin',
+    component: SignInComponent
   }
-
-]
+];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes
 })
 
