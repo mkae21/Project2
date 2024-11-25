4c6abfe68dd2a9150921b49c4fb58bcdf052f753
mkae21 <bbuchu123@gmail.com>
2024-11-25 12:23:30 +0900

    Update HomeWishlistComponent.vue

--- project2/src/components/home/wishlist/HomeWishlistComponent.vue
+++ project2/src/components/home/wishlist/HomeWishlistComponent.vue
@@ -3,13 +3,13 @@
 </template>
 
 <script>
-import MovieWishlist from '@/views/home-wishlist/MovieWishlistComponent.vue'; // 파일 경로를 프로젝트에 맞게 수정
+import MovieWishlist from '@/views/home-wishlist/MovieWishlistComponent.vue' // 파일 경로를 프로젝트에 맞게 수정
 
 export default {
-    name: 'HomeWishlistComponent',
-    components: {
+  name: 'HomeWishlistComponent',
+  components: {
     MovieWishlist
-    }
+  }
 }
 </script>
 
