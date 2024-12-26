8e11d4e2aa6ab6473c78af9dee98237b2732a7c8
mkae21 <bbuchu123@gmail.com>
2024-12-26 21:15:39 +0900

    test

--- project2/src/router/index.js
+++ project2/src/router/index.js
@@ -1,12 +1,12 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'; // Web History 모드 사용
 import HomeComponent from '../components/home/HomeComponent.vue';
 import HomeMainComponent from '../components/home/main/HomeMainComponent.vue';
 import SignInComponent from '../components/sign-in/SignInComponent.vue';
 import HomePopularComponent from '../components/home/popular/HomePopularComponent.vue';
 import HomeWishlistComponent from '../components/home/wishlist/HomeWishlistComponent.vue';
 import HomeSearchComponent from '../components/search/HomeSearchComponent.vue';
-import { toast } from 'vue3-toastify'
-import 'vue3-toastify/dist/index.css'
+import { toast } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
 
 const routes = [
   {
@@ -33,14 +33,14 @@ const routes = [
     meta: { requiresAuth: true }, // 인증이 필요한 라우트
   },
   {
-    path: '/signin', 
+    path: '/signin',
     component: SignInComponent,
   },
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes
+  history: createWebHistory(), // Web History 모드로 변경
+  routes,
 });
 
 // 전역 네비게이션 가드 추가
@@ -49,7 +49,7 @@ router.beforeEach((to, from, next) => {
 
   if (to.matched.some((record) => record.meta.requiresAuth)) {
     if (!isLoggedIn) {
-      toast('로그인이 필요합니다.', { type: 'error', autoClose: 3000 })
+      toast('로그인이 필요합니다.', { type: 'error', autoClose: 3000 });
       return next('/signin'); // 로그인 페이지로 리다이렉트
     }
   }
