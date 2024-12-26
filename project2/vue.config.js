6c8548c5d19f89db9fcf770730807008bc518b73
mkae21 <bbuchu123@gmail.com>
2024-12-26 16:41:49 +0900

    Update: dev, prod 환경

--- project2/vue.config.js
+++ project2/vue.config.js
@@ -9,4 +9,12 @@ module.exports = defineConfig({
   lintOnSave: false,
   publicPath: '/Project2',
   outputDir: 'dist'
+
+  // 개발 서버 설정 추가
+  devServer: {
+    host: process.env.VUE_APP_IP_ADDRESS, // IP 주소, 모든 네트워크 인터페이스에서 접근 가능
+    port: process.env.VUE_APP_PORT,      // 사용할 포트 번호
+    https: false,    // HTTP 프로토콜 사용 (필요 시 true로 설정)
+    open: true,      // 서버 시작 시 기본 브라우저 열기
+  },
 });
